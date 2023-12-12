70453e6f56a5dae423c6c9a0d58368191e678d93
sangwonmoonkr <san4865@naver.com>
2023-12-12 09:10:19 +0900

    crosschain test update

--- shylock_compound/test/unit/Crosschain.t.sol
+++ shylock_compound/test/unit/Crosschain.t.sol
@@ -1,22 +1,27 @@
 
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.22;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 import {Client} from "@chainlink/contracts-ccip/ccip/libraries/Client.sol";
+import "../../src/utils/SimpleERC2771Context.sol";
 
-
-contract ReciveContract {
+contract ReciveContract is SimpleERC2771Context{
   address public receiveGateway;
   function setReciverGateway(address _receiveGateway) public {
     receiveGateway = _receiveGateway;
   }
 
+  function trustedForwarder() override public view virtual returns (address) {
+        return receiveGateway;
+    }
+
+
   event Ping(string text, address sender);
   function ping(string memory text) public {
-    emit Ping(text, msg.sender);
+    emit Ping(text, _msgSender());
   }
 }
 
@@ -50,6 +55,35 @@ contract ReceiveGateway {
     }
 }
 
+
+contract SenderGateway {
+    address public receiveGateway;
+    function setReciverGateway(address _receiveGateway) public {
+        receiveGateway = _receiveGateway;
+    }
+    address public router;
+    function setRouter(address _router) public {
+        router = _router;
+    }
+
+    function sendMessage(
+        address receiver,
+        bytes memory data
+    ) public {
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver), // ABI-encoded receiver contract address
+            data: data,
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 200_000, strict: false}) // Additional arguments, setting gas limit and non-strict sequency mode
+            ),
+            feeToken: address(0) // Setting feeToken to LinkToken address, indicating LINK will be used for fees
+        });
+        
+        Router(router).deliver(receiveGateway, evm2AnyMessage);
+    }
+}
+
 contract Router {
   //   struct Any2EVMMessage {
   //   bytes32 messageId; // MessageId corresponding to ccipSend on source.
@@ -71,38 +105,16 @@ contract Router {
 }
 
 contract Sender {
-    address public receiveGateway;
-    function setReciverGateway(address _receiveGateway) public {
-        receiveGateway = _receiveGateway;
+    address public senderGateway;
+    function setSenderGateway(address _senderGateway) public {
+        senderGateway = _senderGateway;
     }
-    address public router;
-    function setRouter(address _router) public {
-        router = _router;
-    }
-
-    function sendMessage(
-        address receiveGateway,
-        address router,
-        address receiver,
-        bytes memory data
-    ) public {
-        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
-            receiver: abi.encode(receiver), // ABI-encoded receiver contract address
-            data: data,
-            tokenAmounts: new Client.EVMTokenAmount[](0),
-            extraArgs: Client._argsToBytes(
-                Client.EVMExtraArgsV1({gasLimit: 200_000, strict: false}) // Additional arguments, setting gas limit and non-strict sequency mode
-            ),
-            feeToken: address(0) // Setting feeToken to LinkToken address, indicating LINK will be used for fees
-        });
-        
-        Router(router).deliver(receiveGateway, evm2AnyMessage);
-    }
+    
     function triggerPing(address target, string memory text) public {
         bytes4 functionSelector = bytes4(keccak256("ping(string)"));
         bytes memory data = abi.encodeWithSelector(functionSelector, text);
-        // data = abi.encodePacked(data, msg.sender);
-        sendMessage(receiveGateway, router, target, data);
+        data = abi.encodePacked(data, msg.sender);
+        SenderGateway(senderGateway).sendMessage(target, data);
     }
 
 }
@@ -116,10 +128,14 @@ contract CrosschainTest {
     ReciveContract receiveContract = new ReciveContract();
     ReceiveGateway receiveGateway = new ReceiveGateway();
     receiveContract.setReciverGateway(address(receiveGateway));
+    SenderGateway senderGateway = new SenderGateway();
+    senderGateway.setReciverGateway(address(receiveGateway));
+    senderGateway.setRouter(address(new Router()));
+
+
     Sender sender = new Sender();
-    receiveGateway.setFromToConnection(address(sender), address(receiveContract));
-    sender.setReciverGateway(address(receiveGateway));
-    sender.setRouter(address(new Router()));
+    sender.setSenderGateway(address(senderGateway));
+    receiveGateway.setFromToConnection(address(senderGateway), address(receiveContract));
     sender.triggerPing(address(receiveContract), "hello");
   }
   
